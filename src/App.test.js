2802b260dbe20178c442ede2becfc7d5d74ee594
Tom Sturge <tomsturge+github@gmail.com>
2019-08-16 13:27:03 +0100

    Update travis config

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { render, cleanup } from '@testing-library/react';
+
+import App from './App';
+
+afterEach(cleanup);
+
+describe('App', () => {
+  it('should display the content', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
